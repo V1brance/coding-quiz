38ab7c676ee74a4819b5dd1e37415bcdd894090c
Neon Riot <sbrookshire99@gmail.com>
2021-04-25 19:45:51 -0500

    Added functionality to Go Back button when viewing highscores

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,8 @@
 var highscoreButton = document.querySelector("#view-highscore");
 var mainHeader = document.querySelector("#main-title");
 var textArea = document.querySelector("#text-fill");
+var startButton = document.querySelector("#start-button");
+var buttonArea = document.querySelector(".button-area");
 
 //key value pair list for the highscores
 //Remidner, highscoreList[newName] = score will add new item to object
@@ -9,6 +11,9 @@ var highscoreList = {};
 //Changes elements on the page to display the highscores and their scores
 //Includes buttons to go back and clear the highscores
 function viewHighscores(event) {
+  var clearButton;
+  var backButton;
+
   //prevent default behavior of refreshing page on click
   event.preventDefault();
   //Change the title to read Highscore
@@ -18,29 +23,47 @@ function viewHighscores(event) {
   //Move the title off to the left side of the page
   mainHeader.setAttribute("style", "text-align: left");
   //removes the start quiz button
-  document
-    .querySelector("#start-button")
-    .setAttribute("style", "display: none");
-  addButtons();
+  startButton.setAttribute("style", "display: none");
+
+  [clearButton, backButton] = addButtons();
+  backButton.addEventListener("click", function () {
+    reset(buttonArea, clearButton, backButton);
+  });
 }
 
+//Function which adds the new buttons to the screen
 function addButtons() {
   var clearButton = document.createElement("BUTTON");
   clearButton.innerHTML = "Clear Highscores";
   clearButton.setAttribute(
     "style",
-    "font-size: 17px; margin-left: 1vw; margin-right: 1vw"
+    "font-size: 17px; margin-left: 1vw; margin-right: 1vw",
+    "id",
+    "clear-button"
   );
   var backButton = document.createElement("BUTTON");
   backButton.innerHTML = "Go Back";
-  backButton.setAttribute("style", "margin-left: 1vw; margin-right: 1vw");
-
-  var buttonArea = document.querySelector(".button-area");
-
-  console.log(buttonArea);
+  backButton.setAttribute(
+    "style",
+    "margin-left: 1vw; margin-right: 1vw",
+    "id",
+    "back-button"
+  );
 
   buttonArea.appendChild(clearButton);
   buttonArea.appendChild(backButton);
+  return [clearButton, backButton];
+}
+
+//Returns the page back to its original appearance
+function reset(area, clear, back) {
+  mainHeader.textContent = "Coding Quiz Challenge!";
+  mainHeader.setAttribute("style", "text-align: center");
+  textArea.textContent =
+    "Answer as many of the following questions as you can in the next 60 seconds! Every wrong answer takes away 10 seconds. Compete against the high scores and see who is the greatest coder!";
+  startButton.setAttribute("style", "display: inline");
+  area.removeChild(clear);
+  area.removeChild(back);
 }
 
 highscoreButton.addEventListener("click", viewHighscores);
